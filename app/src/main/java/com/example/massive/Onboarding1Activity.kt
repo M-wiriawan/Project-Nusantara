00041a5b0b7e661ccf39eba963f2416dd05d9eee
Wiriawan <mwiriawannst@gmail.com>
2023-11-20 00:19:00 +0700

    Penambahan splesh dan Onboarding

---
+++ app/src/main/java/com/example/massive/Onboarding1Activity.kt
@@ -0,0 +1,39 @@
+package com.example.massive
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class Onboarding1Activity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding1)
+
+        val btnSelanjutnya: Button = findViewById(R.id.btn_slnjut)
+        btnSelanjutnya.setOnClickListener(this)
+
+        val btnLewat: Button = findViewById(R.id.btn_lwt)
+        btnLewat.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+
+            R.id.btn_slnjut -> {
+                val intent = Intent (this@Onboarding1Activity, Onboarding2Activity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.btn_lwt-> {
+                val intent = Intent (this@Onboarding1Activity, Onboarding2Activity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+
+
+}
